ce2efa3ce7f545976560cce44d3b539d10a57b54
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-10 21:44:25 -0300

    chore: adding fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log("hello");
+import fastify from "fastify";
+
+const PORT = 3333;
+export const app = fastify();
+
+app.get("/", (request, reply) => {
+  return reply.send({ message: "hello" });
+});
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: PORT,
+  })
+  .then(() => {
+    console.log(`Server is running ${PORT}`);
+  })
+  .catch((error) => {
+    if (error instanceof Error) {
+      console.error(error.message);
+      process.exit(1);
+    }
+  });
