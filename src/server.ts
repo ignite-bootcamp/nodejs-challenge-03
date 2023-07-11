c899d4b7b814abc1dc235d69c6d48a00e9ab846b
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-10 21:53:16 -0300

    chore: adding eslint

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const PORT = 3333;
 export const app = fastify();
 
-app.get("/", (request, reply) => {
-  return reply.send({ message: "hello" });
+app.get('/', (request, reply) => {
+  return reply.send({ message: 'hello' });
 });
 
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: PORT,
   })
   .then(() => {
     console.log(`Server is running ${PORT}`);
   })
-  .catch((error) => {
+  .catch(error => {
     if (error instanceof Error) {
       console.error(error.message);
       process.exit(1);
