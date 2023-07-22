cbcd337129a9c0cbdec94f45aa22465660a0ff04
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-22 20:21:22 -0300

    tests: created tests for create pet

--- src/use-cases/create-pet.spec.ts
+++ src/use-cases/create-pet.spec.ts
@@ -4,6 +4,8 @@ import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 import { InMemoryImagesRepository } from '@/repositories/in-memory/in-memory-images-repository';
 import { CreatePetUseCase } from './create-pet';
+import { hash } from 'bcryptjs';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
 let orgsRepository: InMemoryOrgsRepository;
 let imagesRepository: InMemoryImagesRepository;
@@ -22,7 +24,59 @@ describe('Register org use case', () => {
     );
   });
 
-  it.todo('should create a pet');
+  it('should create a pet', async () => {
+    const { id } = await orgsRepository.create({
+      email: 'johndoe@example.com',
+      cellphone: '84999999999',
+      latitude: -5.8147019,
+      longitude: -35.2135645,
+      address: 'Rua teste 457',
+      owner: 'John Doe',
+      cep: '00000-000',
+      uf: 'RN',
+      city: 'Natal',
+      password_hash: await hash('123456', 6),
+    });
 
-  it.todo('should not create a pet with invalid org');
+    const { pet } = await sut.execute({
+      org_id: id,
+      requirements: ['precisa de carinho'],
+      energy: 'LOW',
+      about: 'Um cachorrinho legal',
+      size: 'SMALL',
+      name: 'Jorginho',
+      age: 'NEW_BORN',
+      images: [
+        {
+          publicId: 'mocked-public-id',
+          version: '1',
+          format: 'png',
+        },
+      ],
+    });
+
+    expect(pet.id).toEqual(expect.any(String));
+    expect(pet.org_id).toEqual(id);
+  });
+
+  it.todo('should not create a pet with invalid org', async () => {
+    await expect(() => {
+      return sut.execute({
+        org_id: 'invalid-org-id',
+        requirements: ['precisa de carinho'],
+        energy: 'LOW',
+        about: 'Um cachorrinho legal',
+        size: 'SMALL',
+        name: 'Jorginho',
+        age: 'NEW_BORN',
+        images: [
+          {
+            publicId: 'mocked-public-id',
+            version: '1',
+            format: 'png',
+          },
+        ],
+      });
+    }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
 });
