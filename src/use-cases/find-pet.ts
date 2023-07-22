8f6ef5390fd4df7deee921d65ff395421f2e1fd4
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-22 20:42:48 -0300

    feat: created find-pet use case

---
+++ src/use-cases/find-pet.ts
@@ -0,0 +1,18 @@
+import { PetsRepository } from '@/repositories/pets-repository';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+export class FindPetUseCase {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute(petId: string) {
+    const pet = await this.petsRepository.findById(petId);
+
+    if (!pet) {
+      throw new ResourceNotFoundError();
+    }
+
+    return {
+      pet,
+    };
+  }
+}
