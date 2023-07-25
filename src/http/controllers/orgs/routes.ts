3b9d0e4ab5ba2b7779997720abbf72a22b29af69
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-24 22:41:59 -0300

    feat: created refresh token endpoint

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './register';
+import { refresh } from './refresh';
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', register);
+  app.post('/token/refresh', refresh);
 }
