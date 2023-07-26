1877dcd8dbd217c40fee63b575f7ca199bf73d94
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-07-26 04:12:12 -0300

    feat: created create pet endpoint

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { verifyJwt } from '@/http/middlewares/verify-jwt';
 import { find } from './find';
+import { create } from './create';
 
 export async function petsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
 
   app.get('/pets/:id', find);
+  app.post('/pets/:org_id', create);
 }
